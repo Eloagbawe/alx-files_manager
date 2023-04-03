acc97322e3ef3fdd8ccb768151df0018005b72ea
Eloagbawe <eloagbawe@gmail.com>
2023-04-03 02:24:34 +0100

    add routes for file upload and view

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import routes from './routes';
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
 app.listen(port, () => console.log(`Server running on ${port}`));
